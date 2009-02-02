42b5c834ad0b0d4bb97d05d6a8b06188ea1ad795
Christopher Giroir <kelsin@valefor.com>
2009-02-01 17:30:11 -0800

    Initial work

---
+++ db/migrate/20090202003509_create_guests.rb
@@ -0,0 +1,16 @@
+class CreateGuests < ActiveRecord::Migration
+    def self.up
+        create_table :guests do |t|
+            t.number "number"
+            t.string "name"
+            t.text "address"
+            t.string "key"
+            t.boolean "attending"
+            t.timestamps
+        end
+    end
+
+    def self.down
+        drop_table :guests
+    end
+end
