5a3ef8f19f32455ed3f9287ae9a64aebd0728665
Christopher Giroir <kelsin@valefor.com>
2009-04-25 09:05:03 -0700

    New admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
-    map.root :controller => "pages", :action => "wedding"
+  map.root :controller => "pages", :action => "wedding"
 
-    map.location 'location', :controller => "pages", :action => "location"
-    map.engagement 'engagement', :controller => "pages", :action => "engagement"
-    map.chriscait 'chriscait', :controller => "pages", :action => "chriscait"
-    map.registries 'registries', :controller => "pages", :action => "registries"
+  map.location 'location', :controller => "pages", :action => "location"
+  map.engagement 'engagement', :controller => "pages", :action => "engagement"
+  map.chriscait 'chriscait', :controller => "pages", :action => "chriscait"
+  map.registries 'registries', :controller => "pages", :action => "registries"
 
   map.admin 'admin', :controller => "guests", :action => "index"
 
-    map.resources :guests, :path_prefix => '/admin'
-    map.resources :categories, :path_prefix => '/admin'
-    map.resources :rsvp
+  map.resources :guests, :path_prefix => '/admin'
+  map.resources :categories, :path_prefix => '/admin'
+  map.resources :songs, :path_prefix => '/admin'
+  map.resources :rsvp
 
-    map.connect ':controller/:action/:id'
-    map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
 end
