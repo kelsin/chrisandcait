2dd03706cb63fa518249ef877093224c68d91fab
Christopher Giroir <kelsin@valefor.com>
2011-06-14 09:56:51 -0400

    Changes to how we're deploying this

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source :gemcutter
 gem "rails", "2.3.5"
 gem "mysql"
 gem "haml"
+
+gem "capistrano", :group => :development
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 require 'bundler/capistrano'
 
-set :application, "chrisandcait"
+set :application, "chrisandcait.com"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "~/apps/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
@@ -15,24 +15,21 @@ set :repository,  "kelsin@www.chrisandcait.com:~/repos/chrisandcait.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
 
+set :use_sudo, false
 set :user, 'kelsin'
 set :ssh_options, { :forward_agent => true }
- 
-role :app, "www.chrisandcait.com"
-role :web, "www.chrisandcait.com"
-role :db,  "www.chrisandcait.com", :primary => true
+
+role :app, "stalker.kelsin.net"
+role :web, "stalker.kelsin.net"
+role :db,  "stalker.kelsin.net", :primary => true
 
 namespace :deploy do
   task :start, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
   end
 
   task :stop, :roles => :app do
-    # Do nothing.
   end
 
-  desc "Restart Application"
   task :restart, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
   end
 end
