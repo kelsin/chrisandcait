6fec8b34d7575abdbb1832a8104c00beebcc63e2
Christopher Giroir <kelsin@valefor.com>
2014-05-18 21:35:30 -0700

    Removing capistrano

--- config/environment.rb
+++ config/environment.rb
@@ -74,7 +74,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['POSTMARK_SMTP_SERVER'],
   :user_name      => ENV['POSTMARK_API_KEY'],
   :password       => ENV['POSTMARK_API_KEY'],
-  :domain         => 'yourapp.heroku.com',
+  :domain         => 'chrisandcait.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
